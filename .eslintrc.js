bb71488e4e92b333afe066f0173eb1f0a1d5e22f
Miguel Prada <mapra99@gmail.com>
2023-01-29 19:53:28 -0500

    add ignorePatterns rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,13 +14,11 @@ module.exports = {
     tsconfigRootDir: __dirname,
     createDefaultProgram: true,
   },
+  ignorePatterns: ['node_modules/**'],
   settings: {
     'import/resolver': {
       // See https://github.com/benmosher/eslint-plugin-import/issues/1396#issuecomment-575727774 for line below
       node: {},
-      webpack: {
-        config: require.resolve('./.erb/configs/webpack.config.eslint.ts'),
-      },
       typescript: {},
     },
     'import/parsers': {
