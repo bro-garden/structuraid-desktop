28e2cd63577526248e6dc8499f11fe3b87283b4a
immprada <im.mprada@gmail.com>
2023-01-31 17:14:51 -0500

    delete button example component

--- src/renderer/components/button/button.tsx
+++
@@ -1,9 +0,0 @@
-import type { ButtonProps } from './types';
-
-const Button = ({ x }: ButtonProps) => {
-  const style = x === 'sm' ? 'text-sm' : 'text-xl';
-
-  return <h1 className={`text-black ${style}`}>HOLI {x}</h1>;
-};
-
-export default Button;
