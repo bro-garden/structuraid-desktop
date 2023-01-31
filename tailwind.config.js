4e1d0a06bb010e27ffed70c6a1e007fb96a566b5
immprada <im.mprada@gmail.com>
2023-01-31 16:51:22 -0500

    upgrade tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,39 @@
 module.exports = {
   content: ['./src/**/*.{html,js,tsx,ts}'],
-  theme: {},
+  theme: {
+    fontFamily: {
+      sans: ['Nunito', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    fontSize: {
+      '4xl': [
+        '3rem',
+        {
+          lineHeight: '1.4',
+        },
+      ],
+      '3xl': [
+        '2.25rem',
+        {
+          lineHeight: '1.4',
+        },
+      ],
+      '2xl': [
+        '1.75rem',
+        {
+          lineHeight: '1.2',
+        },
+      ],
+      l: [
+        '1.25rem',
+        {
+          lineHeight: '1.2',
+        },
+      ],
+      s: '1rem',
+      xs: '0.75rem',
+    },
+  },
   variants: {},
   plugins: [],
 };
