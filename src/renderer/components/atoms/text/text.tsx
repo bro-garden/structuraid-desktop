9c54eaabb1d31acf42718ec77c5177c00ef6827e
immprada <im.mprada@gmail.com>
2023-01-31 17:10:08 -0500

    add Text atom

--- src/renderer/components/atoms/text/text.tsx
+++ src/renderer/components/atoms/text/text.tsx
@@ -1,9 +1,74 @@
-import type { TextProps } from './types';
+import type {
+  TextProps,
+  TextTag,
+  TextUsage,
+  DefaultTags,
+  TextVariant,
+} from './types';
 
-const Text = ({ x }: TextProps) => {
-  const style = x === 'sm' ? 'text-sm' : 'text-xl';
+const DEFAULT_TAGS: DefaultTags = {
+  'big-title': 'h1',
+  'typical-title': 'h2',
+  'section-title': 'h3',
+  'sub-title': 'h4',
+  'content-body': 'p',
+  'body-small': 'p',
+};
 
-  return <h1 className={`text-black ${style}`}>HOLI TEA {x}</h1>;
+const buildStylesFor = (variantName: TextVariant, as: TextUsage): string => {
+  let styles = ['font-sans'];
+
+  if (variantName === 'bold') {
+    styles = styles.concat(['font-bold']);
+  } else if (variantName === 'regular') {
+    styles = styles.concat(['font-regular']);
+  }
+
+  switch (as) {
+    case 'big-title': {
+      styles = styles.concat(['text-4xl']);
+      break;
+    }
+    case 'typical-title': {
+      styles = styles.concat(['text-3xl']);
+      break;
+    }
+    case 'section-title': {
+      styles = styles.concat(['text-2xl']);
+      break;
+    }
+    case 'sub-title': {
+      styles = styles.concat(['text-l']);
+      break;
+    }
+    case 'content-body': {
+      styles = styles.concat(['text-s']);
+      break;
+    }
+    case 'body-small': {
+      styles = styles.concat(['text-xs']);
+      break;
+    }
+    default: {
+      styles = styles.concat(['text-s']);
+      break;
+    }
+  }
+
+  return styles.join(' ');
+};
+
+const resolveElement = (as: TextUsage): TextTag => {
+  return 'p' || DEFAULT_TAGS[as];
+};
+
+const Text = ({ as, variant, className, children }: TextProps) => {
+  const Element = resolveElement(as);
+  const styles = buildStylesFor(variant, as);
+
+  return (
+    <Element className={`${styles} ${className || ''}`}>{children}</Element>
+  );
 };
 
 export default Text;
--- src/renderer/components/atoms/text/types.ts
+++ src/renderer/components/atoms/text/types.ts
@@ -1,3 +1,27 @@
+import type { ReactNode } from 'react';
+
+export type TextTag = 'h1' | 'h2' | 'h3' | 'h4' | 'span' | 'p';
+export type TextUsage =
+  | 'big-title'
+  | 'typical-title'
+  | 'section-title'
+  | 'sub-title'
+  | 'content-body'
+  | 'body-small';
+export type TextVariant = 'bold' | 'regular';
+
 export interface TextProps {
-  x: string;
+  as: TextUsage;
+  variant: TextVariant;
+  className?: string;
+  children: ReactNode;
+}
+
+export interface DefaultTags {
+  'big-title': TextTag;
+  'typical-title': TextTag;
+  'section-title': TextTag;
+  'sub-title': TextTag;
+  'content-body': TextTag;
+  'body-small': TextTag;
 }
