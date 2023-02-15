4ea629357c4c1feccda401b534fe3cba52fdcb83
Miguel Prada <mapra99@gmail.com>
2023-02-15 12:13:57 -0500

    rename files

--- src/renderer/components/atoms/text/helper/index.ts
+++ src/renderer/components/atoms/text/helper/index.ts
@@ -1,3 +1,3 @@
-export { default as buildStylesFor } from './buildStylesFor';
-export { default as returnTextColor } from './returnTextColor';
-export { default as resolveElement } from './resolveElement';
+export { default as buildStylesFor } from './build-styles-for';
+export { default as returnTextColor } from './return-text-color';
+export { default as resolveElement } from './resolve-element';
