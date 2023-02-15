8f8998928f4a8458673d70713d19a91d2882c656
immprada <im.mprada@gmail.com>
2023-02-15 11:03:41 -0500

    update types

--- src/renderer/components/atoms/button/types.ts
+++ src/renderer/components/atoms/button/types.ts
@@ -8,12 +8,6 @@ export type TagOptions = 'button' | 'link' | 'anchor';
 export type ButtonType = 'primary' | 'secondary' | 'warning';
 export type ButtonVariants = 'big' | 'small';
 
-export interface ButtonProps {
-  useTag: TagOptions;
-  children: ReactNode | string;
-  props: SimpleButtonProps | AnchorProps;
-}
-
 export interface SimpleButtonProps
   extends ButtonHTMLAttributes<HTMLButtonElement> {
   className?: string;
