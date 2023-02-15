1f0cd9284cff3da5059d19692b2139bec6a7dea6
Miguel Prada <mapra99@gmail.com>
2023-02-14 20:48:23 -0500

    wip

--- src/renderer/components/atoms/dropdown/dropdown.tsx
+++ src/renderer/components/atoms/dropdown/dropdown.tsx
@@ -1,7 +1,7 @@
 import type { DropdownProps } from './types';
 
 const Dropdown = ({ color, items }: DropdownProps<unknown>) => {
-  return <div className="" />;
+  return <div className="bg-blue-white" />;
 };
 
 export default Dropdown;
