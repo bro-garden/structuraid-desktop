e87b471649a634f95192fad39eb5b7d62933c820
immprada <im.mprada@gmail.com>
2023-02-15 12:52:47 -0500

    update button indexes

--- src/renderer/components/atoms/button/helper/index.ts
+++ src/renderer/components/atoms/button/helper/index.ts
@@ -1,2 +1,3 @@
 export { default as buildStylesForAnchor } from './build_styles_for_anchor';
 export { default as buildStylesForButton } from './build_styles_for_button';
+export { default as buildStylesForLink } from './build_styles_for_link';
