4836a78620eef85ffa36af49a2e337a8f20e93bc
Miguel Prada <mapra99@gmail.com>
2023-02-15 12:17:59 -0500

    deleted files

--- src/renderer/pages/home/Home.test.tsx
+++
@@ -1,9 +0,0 @@
-import '@testing-library/jest-dom';
-import { render } from '@testing-library/react';
-import Home from './home';
-
-describe('Home', () => {
-  it('should render', () => {
-    expect(render(<Home />)).toBeTruthy();
-  });
-});
