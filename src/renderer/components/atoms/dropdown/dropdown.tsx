c34bce5faed4486a2d91b7dbb63a1ce51d0936ee
Miguel Prada <mapra99@gmail.com>
2023-02-15 14:48:25 -0500

    export dropdown in index file

--- src/renderer/components/atoms/dropdown/dropdown.tsx
+++ src/renderer/components/atoms/dropdown/dropdown.tsx
@@ -1,5 +1,4 @@
 import { useState } from 'react';
-import { on } from 'events';
 import type { Item, DropdownProps } from './types';
 
 const Dropdown = ({
--- src/renderer/components/atoms/index.ts
+++ src/renderer/components/atoms/index.ts
@@ -1,4 +1,5 @@
 /* eslint-disable import/prefer-default-export */
 export { default as Text } from './text';
 export { default as Input } from './input';
+export { default as Dropdown } from './dropdown';
 export * from './button';
