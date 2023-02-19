cbe23e8d604888c1e106653ae0f7206c5c8b3260
Miguel Prada <mapra99@gmail.com>
2023-02-19 17:32:53 -0500

    feat(ui): build toggle component

--- src/renderer/components/atoms/index.ts
+++ src/renderer/components/atoms/index.ts
@@ -2,4 +2,5 @@
 export { default as Text } from './text';
 export { default as Input } from './input';
 export { default as SelectionList } from './selection-list';
+export { default as Toggle } from './toggle';
 export * from './button';
